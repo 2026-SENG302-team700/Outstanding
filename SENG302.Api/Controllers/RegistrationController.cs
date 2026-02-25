aea3ba10cce7cf224a165edc4aa049e2d8753c00
Shivam Pandya <spa293@uclive.ac.nz>
2026-02-25 17:00:47 +1300

    Fixed issue of build failing

--- SENG302.Api/Controllers/RegistrationController.cs
+++ SENG302.Api/Controllers/RegistrationController.cs
@@ -4,24 +4,24 @@ namespace SENG302.Api.Controllers;
 
 public class RegistrationController : ControllerBase
 {
-    [HttpGet (int:id)]
+    [HttpGet ("{id:int}")]
     public async Task<ActionResult<User>> getUser(int id)
     {
-        
+        return new User();
     }
 
     [HttpPost]
     [ValidateAntiForgeryToken]
     public async Task<ActionResult<User>> CreateUser([FromBody] User user)
     {
-        
+        return new User();
     }
 
     [HttpPut("{id:int}")]
     [ValidateAntiForgeryToken]
     public async Task<ActionResult<User>> UpdateUser(int id, [FromBody] User user)
     {
-        
+        return new User();
     }
 }
 
