29c5070ef1dcf379f4d1c214b5766dff030101a1
Shaun <sms251@uclive.ac.nz>
2026-02-26 14:42:11 +1300

    Created task list creation screen

--- SENG302.App/src/routes/home/new-list/+page.svelte
+++ SENG302.App/src/routes/home/new-list/+page.svelte
@@ -6,16 +6,43 @@
 
     let loading = $state(false);
     let error = $state("");
+    let name = $state("");
+
+    async function createList() {
+        // TODO - implement list creation logic
+    }
 </script>
 
 <div class="container">
-    <h1 class="text-center mb-4">Task List Creation Screen (WIP)</h1>
-    <button
-        type="button"
-        class="btn btn-secondary"
-        on:click={() => goto(resolve("/home"))}
-        >Cancel
-    </button>
+    <h1 class="text-center mb-4">Name your new task list</h1>
+    <div class="mb-3">
+        <button
+            type="button"
+            class="btn btn-secondary"
+            on:click={() => goto(resolve("/home"))}
+            >Cancel
+        </button>
+    </div>
+    <form on:submit={createList}>
+        <div class="mb-3">
+            <input
+                type="text"
+                class="form-control"
+                placeholder="Name"
+                bind:value={name}
+                disabled={loading}
+            />
+        </div>
+        <div>
+            <button
+                type="submit"
+                class="btn btn-primary w-100"
+                disabled={loading}
+            >
+                {loading ? "Creating..." : "Create List"}
+            </button>
+        </div>
+    </form>
 </div>
 
 <style>
