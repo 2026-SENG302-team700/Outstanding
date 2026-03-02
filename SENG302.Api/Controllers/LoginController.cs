6c45c813526af9ff59f5754c7e005e0ebaf4599e
Shawn Neal <sne87@uclive.ac.nz>
2026-03-02 16:19:55 +1300

    document: adding docstrings to my created functions, methods and classes

--- SENG302.Api/Controllers/LoginController.cs
+++ SENG302.Api/Controllers/LoginController.cs
@@ -16,6 +16,12 @@ public class LoginController: ControllerBase
         _userService = userService;
     }
 
+
+    /// <summary>
+    /// Check to ensure the provided email and password match a registered user
+    /// </summary>
+    /// <param name="userCredentials">a UserCredentials object provided by the frontend containing the details used for an attempted login</param>
+    /// <returns>a Task<ActionResult<User>></returns>
     [HttpPost]
     [ConditionalValidateAntiForgeryToken]
     public async Task<ActionResult<User>> CheckCredentials([FromBody] UserCredentials userCredentials) 
--- SENG302.Api/Models/Entities/UserCredentials.cs
+++ SENG302.Api/Models/Entities/UserCredentials.cs
@@ -2,6 +2,9 @@ using Microsoft.Net.Http.Headers;
 
 namespace SENG302.Api.Models.Entities;
 
+/// <summary> 
+/// Object used to store the email and the password entered in the frontend to be processed for login verification
+/// </summary>
 public class UserCredentials {
     public string Email { get; set; }
     public string PasswordKey {get; set;}
