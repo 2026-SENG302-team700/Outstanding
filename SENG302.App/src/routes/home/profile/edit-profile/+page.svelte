3375528547c512485805ee1c9675aa7be4fcf28f
Shaun <sms251@uclive.ac.nz>
2026-03-11 11:41:00 +1300

    Feat: Update profile form that loads users data into it

--- SENG302.App/src/routes/home/profile/edit-profile/+page.svelte
+++ SENG302.App/src/routes/home/profile/edit-profile/+page.svelte
@@ -1,14 +1,83 @@
 <script lang="ts">
     // import defaultLogo from '$team-700/SENG302.App/static/defaultProfile.png/';
     import { onMount } from "svelte";
-    import type { Book } from "$lib/types";
     import { goto } from "$app/navigation";
     import { resolve } from "$app/paths";
     import { fetchWithCsrf } from "$lib/csrf";
     import { countries } from "$lib/country/countries";
 
+    let displayName = $state("");
     let email = $state("");
-    let username = $state("");
+    let country = $state("");
 
-    onMount(() => {});
+    onMount(() => {
+        retrieveUserData();
+    });
+
+    /// <summary>
+    /// Gets User email and Username from local storage
+    /// Checks if they non null before assigning them to the reactive variables
+    /// </>summary>
+    async function retrieveUserData() {
+        try {
+            const response = await fetchWithCsrf(resolve(`/api/user`), {
+                method: "GET",
+                credentials: "include",
+            });
+
+            const data = await response.json();
+            if (!response.ok) {
+                email = data.message || "Failed to fetch email.";
+                displayName = data.message || "Failed to fetch username.";
+                goto(resolve("/"));
+                return;
+            }
+            email = data.email;
+            displayName = data.displayName;
+            country = data.country;
+        } catch (err) {
+            email = "Failed to fetch email: " + (err as Error).message;
+            displayName = "Failed to fetch username: " + (err as Error).message;
+            goto(resolve("/"));
+        }
+    }
 </script>
+
+<div class="container">
+    <form>
+        <div class="mb-3">
+            <label for="displayName" class="form-label">Display Name</label>
+            <input
+                type="text"
+                class="form-control"
+                value={displayName}
+                id="displayName"
+            />
+        </div>
+        <div class="mb-3">
+            <label for="userEmail" class="form-label">Email</label>
+            <input
+                type="email"
+                class="form-control"
+                id="userEmail"
+                value={email}
+            />
+        </div>
+        <div class="mb-3">
+            <label for="country" class="form-label">Country</label>
+            <select
+                class="form-control"
+                class:country-select={!country}
+                bind:value={country}
+                id="country"
+            >
+                {#each countries as country}
+                    <option value={country.code}>
+                        {country.name}
+                    </option>
+                {/each}
+            </select>
+        </div>
+        <button type="submit" class="btn btn-primary">Update</button>
+    </form>
+</div>
