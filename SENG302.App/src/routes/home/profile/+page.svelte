673de45fc7f4846e7be3a37e65b92361b192b93b
Shaun <sms251@uclive.ac.nz>
2026-03-11 11:02:39 +1300

    Feat: Added 'Edit Profile' Button to  profile page

--- SENG302.App/src/routes/home/profile/+page.svelte
+++ SENG302.App/src/routes/home/profile/+page.svelte
@@ -44,9 +44,13 @@
 
 <div class="display: flex; flex-direction: row;">
     <div class="profile-box">
+        <button
+            class="btn btn-primary ms-auto"
+            onclick={() => goto(resolve("/home/profile/edit-profile"))}
+            >Edit Profile</button
+        >
         <img
             class="profile-image"
-            ,
             src="/defaultProfile.png"
             alt="No Profile Picture"
         />
