b26a798bdf20acd0058f2d0779320245070d8968
Sam Ladbrook <sla202@uclive.ac.nz>
2026-03-13 14:26:23 +1300

    feat: added bootstrap js to base svelte layout file to ensure we can make use of dynamic bootstrap elements

--- SENG302.App/src/routes/+layout.svelte
+++ SENG302.App/src/routes/+layout.svelte
@@ -2,12 +2,19 @@
 	import 'bootstrap/dist/css/bootstrap.min.css';
 	import '../app.css';
 	import Toast from '$lib/toast/toast.svelte';
+	import { onMount } from 'svelte';
 	
 	interface Props {
 		children?: import('svelte').Snippet;
 	}
 
 	let { children }: Props = $props();
+
+	// added to ensure we can make use of bootstrap js for things like dropdowns
+	onMount(async () => {
+		// @ts-ignore
+		await import('bootstrap');
+	});
 </script>
 
 <Toast />
