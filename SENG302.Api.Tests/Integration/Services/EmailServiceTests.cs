4160b3d8c9143e6f11233d68927392d1255da43a
Sam Ladbrook <sla202@uclive.ac.nz>
2026-03-21 12:24:55 +1300

    feat: removed the email service integration test as it wasnt going to work or be valid

--- SENG302.Api.Tests/Integration/Services/EmailServiceTests.cs
+++ SENG302.Api.Tests/Integration/Services/EmailServiceTests.cs
@@ -1,10 +1,10 @@
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Options;
 using SENG302.Api.Services;
 using Shouldly;
-using Xunit;
-
 
 namespace SENG302.Api.Tests.Integration.Services;
 
@@ -13,29 +13,4 @@ public class EmailServiceTests : BaseIntegrationTestFixture
     private IEmailService ServiceUnderTest => ServiceProvider.GetRequiredService<IEmailService>();
 
     public EmailServiceTests(WebApplicationFactory<Program> webAppFactory) : base(webAppFactory) { }
-
-    [Fact]
-    public async Task SendEmail_ToLocalSmtp_DoesNotThrow()
-    {
-        var settings = Options.Create(new EmailSettings
-        {
-            Host = "localhost",
-            Port = 1025,
-            FromEmail = "noreply.outstanding@gmail.com",
-            Password = ""
-        });
-
-        await Should.NotThrowAsync(async () =>
-        {
-            await ServiceUnderTest.SendEmailAsync(
-                "test@example.com",
-                EmailTemplate.VerifyEmailCode,
-                new Dictionary<string, string>
-                {
-                    ["DISPLAY_NAME"] = "Test",
-                    ["CODE"] = "123456",
-                    ["MINUTES"] = "5"
-                });
-        });
-    }
 }
