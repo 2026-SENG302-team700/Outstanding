7f53d9057979f801da57d7d6e8a796f300a9abdd
Shaun <sms251@uclive.ac.nz>
2026-03-21 16:14:13 +1300

    Feat: When database is created, include admin user with email "admin@outstanding.com" and password "Team700!"

--- SENG302.Api/DataAccess/DatabaseContext.cs
+++ SENG302.Api/DataAccess/DatabaseContext.cs
@@ -1,16 +1,33 @@
 using Microsoft.EntityFrameworkCore;
 using SENG302.Api.Models.Entities;
+using Microsoft.AspNetCore.Identity;
 
 namespace SENG302.Api.DataAccess;
 
 public class DatabaseContext : DbContext
 {
-    public DatabaseContext(DbContextOptions options) : base(options)
-    {
-
-    }
+    public DatabaseContext(DbContextOptions options) : base(options) { }
 
     public DbSet<User> Users { get; set; }
     public DbSet<TaskList> TaskLists { get; set; }
     public DbSet<TaskItem> TaskItems { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Seed a default user
+        var user = new User
+        {
+            Id = 1, // make sure this matches your key type
+            DisplayName = "admin",
+            Email = "admin@outstanding.com",
+            Country = "NZ",
+            EmailVerified = true
+        };
+        PasswordHasher<User> passwordHasher = new();
+        var passwordKey = passwordHasher.HashPassword(user, "Team700!");
+        user.PasswordKey = passwordKey;
+        modelBuilder.Entity<User>().HasData(user);
+    }
 }
