954a6b17ee87d5911e65fcd689eeb82e95d3de37
jsa162 <jsa162@uclive.ac.nz>
2026-03-25 09:36:59 +1300

    fix: the "input a valid date" is now correctly formatted

--- SENG302.App/src/routes/home/task-list/[slug]/create-task/+page.svelte
+++ SENG302.App/src/routes/home/task-list/[slug]/create-task/+page.svelte
@@ -4,7 +4,7 @@
     import { fetchWithCsrf } from "$lib/csrf";
     import { onMount } from "svelte";
     import { addToast } from "$lib/toast/toast";
-    import { validateTaskInput } from "$lib/validity/taskValidity"
+    import { validateTaskInput } from "$lib/validity/taskValidity";
     import DatePicker from "$lib/datepicker/datepicker.svelte";
     import StatusDropdown from "$lib/statusdropdown/statusdropdown.svelte";
 
@@ -23,8 +23,16 @@
         taskStatus: "",
     });
 
+    let datePicker: HTMLInputElement | undefined = $state();
+
     onMount(() => {
         GetList();
+
+        if (datePicker)
+            datePicker.addEventListener("invalid", function (event) {
+                event.preventDefault();
+                errors.dueDate = "Please input a valid date";
+            });
     });
 
     /**
@@ -33,18 +41,18 @@
      */
     async function createTask() {
         const dueDate = new Date(taskDue);
-        
+
         if (dueDate.getFullYear() !== 1) {
             dueDate.setHours(23, 59, 59, 999);
         }
-        
+
         const validationData = validateTaskInput(
             name,
             description,
             dueDate,
-            taskStatus
+            taskStatus,
         );
-        
+
         if (!validationData.isValid) {
             errors.name = validationData.name;
             errors.description = validationData.description;
@@ -52,7 +60,7 @@
             errors.taskStatus = validationData.taskStatus;
             return;
         }
-        
+
         try {
             loading = true;
             const response = await fetchWithCsrf(
@@ -178,9 +186,7 @@
 
         <div class="mb-3 d-flex align-items-center gap-2">
             <label class="form-label mb-0">Status:</label>
-            <StatusDropdown
-                bind:value={taskStatus}
-            />
+            <StatusDropdown bind:value={taskStatus} />
             {#if error}
                 <div class="text-danger mt-1">{errors.taskStatus}</div>
             {/if}
@@ -190,9 +196,11 @@
             <div class="d-flex align-items-center gap-2">
                 <label class="form-label mb-0">Due Date:</label>
                 <DatePicker
-                        bind:value={taskDue}
-                        error={errors.dueDate}
-                        disabled={loading} />
+                    bind:value={taskDue}
+                    error={errors.dueDate}
+                    bind:date={datePicker}
+                    disabled={loading}
+                />
             </div>
             {#if errors.dueDate}
                 <div class="invalid-feedback d-block">
@@ -200,7 +208,7 @@
                 </div>
             {/if}
         </div>
-        
+
         <div>
             <button
                 type="submit"
