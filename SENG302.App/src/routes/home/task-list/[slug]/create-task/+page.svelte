a157d84dcb1c990bad860431da1926140af37aa1
Shawn <sne87@uclive.ac.nz>
2026-04-19 11:30:07 +1200

    removed "resolve" from cancel button

--- SENG302.App/src/routes/home/task-list/[slug]/create-task/+page.svelte
+++ SENG302.App/src/routes/home/task-list/[slug]/create-task/+page.svelte
@@ -10,6 +10,7 @@
     import CancelButton from "$lib/components/cancel-button.svelte";
     import TitleForm from "$lib/components/title-form.svelte";
     import DescriptionForm from "$lib/components/description-form.svelte";
+    import path from "node:path";
 
     let taskStatus = $state(0); // represents the value of the enum in the backend
     let loading = $state(false);
@@ -147,28 +148,19 @@
         </h1>
     </div>
     <div class="mb-3">
-        <button
-                type="button"
-                class="btn btn-secondary"
-                on:click={() => goto(".")}
-        >Cancel
-        </button>
+        <CancelButton path={"."} />
     </div>
     <form on:submit|preventDefault={createTask}>
         <div class="mb-3">
-            <TitleForm
-                bind:name
-                error={errors.name}
-                {loading}
-            />    
+            <TitleForm bind:name error={errors.name} {loading} />
         </div>
 
         <div class="mb-3">
-            <DescriptionForm 
-                    bind:description
-                    error={errors.description}
-                    {loading}
-            />        
+            <DescriptionForm
+                bind:description
+                error={errors.description}
+                {loading}
+            />
         </div>
 
         <div class="mb-3 d-flex align-items-center gap-2">
