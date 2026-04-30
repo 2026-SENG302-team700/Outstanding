c623bc977c35eded31267ed06f036ed24ed0ac1f
Sam Ladbrook <sla202@uclive.ac.nz>
2026-04-30 16:41:40 +1200

    test: created the base acceptance test file and also added AT for U13 AC3

---
+++ SENG302.Api.Tests/Acceptance/Setup/AcceptanceTestFixture.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using SENG302.Api.Tests.Integration;
+using Reqnroll;
+using System.Data;
+using Microsoft.EntityFrameworkCore;
+using SENG302.Api.DataAccess;
+
+namespace SENG302.Api.Tests.Acceptance.Setup;
+
+[Binding]
+public class AcceptanceTestFixture : BaseIntegrationTestFixture
+{
+    public int? CurrentUserId { get; set; }
+    public new HttpClient HttpClient => base.HttpClient;
+    public new IDbContextFactory<DatabaseContext> DbContextFactory => base.DbContextFactory;
+
+    public AcceptanceTestFixture(WebApplicationFactory<Program> factory) : base(factory)
+    {
+    }
+
+    [BeforeScenario]
+    public async Task ScenarioSetup()
+    {
+        await InitializeAsync();
+        CurrentUserId = null;
+    }
+
+    [AfterScenario]
+    public async Task ScenarioTeardown()
+    {
+        await DisposeAsync();
+    }
+}
