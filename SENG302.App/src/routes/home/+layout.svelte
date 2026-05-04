3943050fa327862f80c0906f762dbeda686dea71
Shaun Souness <sms251@uclive.ac.nz>
2026-05-04 13:55:22 +1200

    Revert "Feat: added footer bar with nav to content policy page"
    This reverts commit 498502869d78c29022c8106467b3a644fcc564d1

--- SENG302.App/src/routes/home/+layout.svelte
+++ SENG302.App/src/routes/home/+layout.svelte
@@ -103,20 +103,7 @@
     </div>
   </div>
 </nav>
-
-<div class="page-wrapper container-fluid bg-light py-4">
-  <div class="content">
-    {@render children?.()}
-  </div>
-
-  <footer class="footerBar">
-    <div class="footerContent">
-      <button class="btn btn-secondary" onclick={() => goto(resolve("/home/about"))}>
-        Content Policy
-      </button>
-    </div>
-  </footer>
-</div>
+{@render children?.()}
 
 <style>
   .profile-image {
@@ -150,29 +137,5 @@
       -1px -5px 4px rgba(214, 213, 210, 0.75);
     border-radius: 10px;
     margin-bottom: 20px;
-  }
-
-  .footerBar {
-    margin-top: auto;
-    background-color: white;
-    height: 60px;
-    box-shadow: 0 -2px 6px rgba(214, 213, 210, 0.75);
-    border-radius: 10px;
-    display: flex;
-    align-items: center;
-  }
-
-  .footerContent {
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    padding: 0 15px;
-    align-items: center;
-  }
-
-  .page-wrapper {
-    min-height: 85vh; 
-    display: flex;
-    flex-direction: column;
   }
 </style>
