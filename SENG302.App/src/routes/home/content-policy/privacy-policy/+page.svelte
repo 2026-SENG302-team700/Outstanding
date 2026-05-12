a3d25eb5e6573e20dba0db2db3bb8fd841bf5448
Shivam <spa293@uclive.ac.nz>
2026-05-13 08:30:09 +1200

    Created initial file or component for privacy policy

---
+++ SENG302.App/src/routes/home/content-policy/privacy-policy/+page.svelte
@@ -0,0 +1,15 @@
+﻿<script lang="ts">
+    import { goto } from "$app/navigation";
+    import { resolve } from "$app/paths";
+    import ContentPolicy from "\$lib/policies/content-policy.svelte";
+</script>
+
+<div class="container py-4">
+    <!-- <ContentPolicy/>  Replace with seperate privacy policy component--> 
+    <button
+            class="btn btn-primary mt-3"
+            on:click={() => goto(resolve("/home"))}
+    >
+        Back to Home
+    </button>
+</div>
